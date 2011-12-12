e0cfc52f8c4c6afa21a0841a659757a713a19d26
Shamsul Haque <shamsulsham89@gmail.com>
2011-12-12 19:23:38 +0530

    first commit

---
+++ one_eight_two_182/app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+  has_attached_file :avatar, :styles => { :small => "150x150>", :big => "850x8500>" }, :processors => [:cropper]
+  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  after_update :reprocess_avatar, :if => :cropping?
+
+  def cropping?
+    !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
+  end
+
+  def avatar_geometry(style = :original)
+    @geometry ||= {}
+    @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
+  end
+
+  private
+
+  def reprocess_avatar
+    avatar.reprocess!
+  end
+
+end
---
+++ one_eight_two_182/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imageupload::Application.config.secret_token = 'e097970de1b153874b6852efd56f34c440a159b9d2fec488cc04c5a3d9d5e87870ba23e8df76f5e4fdf641920295e5bb0c4199d93f7a19e9e01b2e3ef7c4cd09'
